ac531833ef26ccea8d542e14ef77084f11a47c75
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-21 09:29:46 +0200

    Implementing Service Layer Methods

--- src/main/java/com/dgmf/service/BookService.java
+++ src/main/java/com/dgmf/service/BookService.java
@@ -1,4 +1,15 @@
 package com.dgmf.service;
 
+import com.dgmf.entity.Book;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookService {
+    // Creates a Book
+    Book createBook(Book book);
+    // Get all the Books
+    List<Book> getAllBooks();
+    // Get a single Book
+    Book getBook(Long id);
 }
--- src/main/java/com/dgmf/service/impl/BookServiceImpl.java
+++ src/main/java/com/dgmf/service/impl/BookServiceImpl.java
@@ -1,8 +1,33 @@
 package com.dgmf.service.impl;
 
+import com.dgmf.entity.Book;
+import com.dgmf.repository.BookRepository;
 import com.dgmf.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Override
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book getBook(Long id) {
+        return bookRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Book Not Found")
+        );
+    }
 }
